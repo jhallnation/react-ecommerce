03f263977f5571fd70d26c2f2cbabe8ac605875c
Jason Hall <jhallnation@gmail.com>
2020-11-25 11:59:40 -0700

    signin and signup components

---
+++ src/components/auth/signin.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class SignIn extends Component {
+  render() {
+    return (
+      <div className='sign-in'>
+        <h1>Sign In</h1>
+        what the hell
+        why is this not showing up
+      </div>
+    )
+  }
+}
