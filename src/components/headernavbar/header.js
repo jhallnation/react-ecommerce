a175466f87c6f034222d9bc030c99fe457c9f04b
Jason Hall <jhallnation@gmail.com>
2021-04-28 14:01:41 -0600

    turned logo into link to home

--- src/components/headernavbar/header.js
+++ src/components/headernavbar/header.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import jh_logo from '../../images/jh_logo.png';
 
 class Header extends Component {
   render(){
     return (
       <div className='header'>
-        <img src={jh_logo} className='logo'/>
+        <Link to='/'>
+          <img src={jh_logo} className='logo'/>
+        </Link>
       </div>
     )
   }
